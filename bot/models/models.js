dc44d539a428c082b0335475843da8240f83109f
Sergey Gornih <seregaland@yandex.ru>
2025-01-15 12:34:47 +0300

    fix worker add

--- bot/models/models.js
+++ bot/models/models.js
@@ -1,7 +1,7 @@
 const sequelize = require('../connections/db')
 const {DataTypes} = require('sequelize')
 
-const UserBot = sequelize.define('userbot', {
+const UserBot = sequelize.define('wuserbot', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     firstname: {type: DataTypes.STRING},
     lastname: {type: DataTypes.STRING},
@@ -26,9 +26,32 @@ const Conversation = sequelize.define('conversation', {
     members: {type: DataTypes.ARRAY(DataTypes.STRING)},
 })
 
+const Worker = sequelize.define('worker', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    userfamily: {type: DataTypes.STRING},
+    username: {type: DataTypes.STRING},
+    phone: {type: DataTypes.STRING},
+    dateborn: {type: DataTypes.STRING},  
+    city: {type: DataTypes.STRING},
+    companys: {type: DataTypes.STRING},
+    stag: {type: DataTypes.STRING},
+    worklist: {type: DataTypes.STRING},
+    chatId: {type: DataTypes.STRING, unique: true},
+    promoId: {type: DataTypes.STRING},
+    from: {type: DataTypes.STRING},
+    avatar: {type: DataTypes.STRING},
+    comment: {type: DataTypes.TEXT}, 
+    rank: {type: DataTypes.INTEGER}, 
+    block: {type: DataTypes.BOOLEAN},
+    deleted: {type: DataTypes.BOOLEAN},
+    newcity: {type: DataTypes.STRING},
+    great: {type: DataTypes.BOOLEAN},
+})
+
 
 module.exports = {
     UserBot, 
     Message, 
     Conversation, 
+    Worker,
 }
